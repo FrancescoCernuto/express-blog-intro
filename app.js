6e6cfe0c244a4dc024512fbe22c8f8dce550995c
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-19 16:05:14 +0100

    add firt item array

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const posts = [
+    {
+      title: "Ciambellone",
+      content: "lorem ipsum sit amet",
+      img: "./public/images/ciambellone.jpeg",
+      tags: ["ricetta", "ciambellone"],
+    },
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
