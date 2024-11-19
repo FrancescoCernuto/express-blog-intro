ba7e2038bef6b26129590399cf55c24d5d7ee699
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-19 16:15:51 +0100

    add array

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ const posts = [
       img: "./public/images/ciambellone.jpeg",
       tags: ["ricetta", "ciambellone"],
     },
+    {
+        title: "Crackers alla barbabietola",
+        content: "lorem ipsum sit amet",
+        img: "./public/images/cracker_barbabietola.jpeg",
+        tags: ["ricetta", "cracker", "barbabietola"],
+      },
+    
+      {
+        title: "Pane fritto dolce",
+        content: "lorem ipsum sit amet",
+        img: "./public/images/pane_fritto_dolce.jpeg",
+        tags: ["ricetta", "pane", "fritto", "dolce"],
+      },
+    
+      {
+        title: "Pasta alla barbabietola",
+        content: "lorem ipsum sit amet",
+        img: "./public/images/pasta_barbabietola.jpeg",
+        tags: ["ricetta", "pasta", "alla", "barbabietola"],
+      },
+    
+      {
+        title: "Torta paesana",
+        content: "lorem ipsum sit amet",
+        img: "./public/images/torta_paesana.jpeg",
+        tags: ["ricetta", "torta", "paesana"],
+      },
+    ];
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
